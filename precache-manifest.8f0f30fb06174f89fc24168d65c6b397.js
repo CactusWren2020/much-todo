da300d51b3d0167e5584b71713d592ec3c0e72b9
Michael W. Cho <elegantspanishguitar@gmail.com>
2020-09-10 21:10:42 -0700

    Add files via upload
    Uploaded files

---
+++ precache-manifest.8f0f30fb06174f89fc24168d65c6b397.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "24735e34e857887877ac",
+    "url": "/static/js/main.24735e34.chunk.js"
+  },
+  {
+    "revision": "038a6e1c9ee1df51d4ba",
+    "url": "/static/js/1.038a6e1c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7828167771a08313ad316d153a96436e",
+    "url": "/index.html"
+  }
+];
